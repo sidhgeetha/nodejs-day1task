eb132f4fd98b3e9da43f7af2e8dc47c265b24bf4
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-03-06 23:57:24 +0000

    chnages updated

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 import express from 'express';
+import fs from 'fs';
+import {format} from 'date-fns';
 
 const app =express();
 const PORT =4000;
 
-app.get('/', (req,res)=>{
+// app.get('/', (req,res)=>{
 
-    res.status(200).json({message:"Hi team,welcome to guvi world"})
+//     res.status(200).json({message:"Hi team,welcome to guvi world"})
 
-})
+// })
+
+
+
+
+app.get("/write", (req, res) => {
+  let today = format(new Date(), "dd-mm-yyyy-hh-mm-ss");
+  console.log("Today:", today);
+
+  const filePath = `TimeStamp/${today}.txt`;
+
+  fs.writeFileSync(filePath, today, "utf8");
+
+  res.status(200).send(`${today}`);
+});
+
+// Read file API
+app.get("/read", (req, res) => {
+  const files = fs.readdirSync("TimeStamp");
+ 
+  const latestFile = files[files.length - 1];
+  const filePath = `TimeStamp/${latestFile}`;
+  let data = fs.readFileSync(filePath, "utf8");
+  res.status(200).send(data);
+});
+
+
+
+
+
 
 
 
